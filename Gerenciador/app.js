23b10e90c2d163a4d30549b9bc4eccdc0de3ffdd
AntonioMontenegro-Glich <antoniolemos2004@gmail.com>
2024-12-12 17:41:56 -0300

    Deploy render

--- Gerenciador/app.js
+++ Gerenciador/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const produtoRoutes = require("./routes/produtoRoute"); 
 require('dotenv').config();
@@ -8,6 +9,7 @@ const app = express();
 
 
 app.use(express.json());
+app.use(cors());
 
 
 const conectarMongoDB = async () => {
