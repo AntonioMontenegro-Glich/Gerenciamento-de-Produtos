bce279d4d06d10acaaf1a381165450eab644aa38
AntonioMontenegro-Glich <antoniolemos2004@gmail.com>
2024-12-13 14:33:31 -0300

    Erro no modelo

--- Gerenciador/models/Produto.js
+++ Gerenciador/models/Produto.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const produtoSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   descricao: { type: String, required: true },
-  quantidade: { type: mongoose.Schema.Types.ObjectId, required: true },
+  quantidade: { type: Number, required: true },
   foto: { type: String, required: false },
 }, { timestamps: true }); // Aqui é o local correto de configuração de timestamps
 
