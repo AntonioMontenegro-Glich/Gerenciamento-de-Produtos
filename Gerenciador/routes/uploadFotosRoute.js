4425f7428ad2ef8e59681724481950919cf295a5
AntonioMontenegro-Glich <antoniolemos2004@gmail.com>
2024-12-14 11:18:46 -0300

    Upload fotos 1.2

--- Gerenciador/routes/uploadFotosRoute.js
+++ Gerenciador/routes/uploadFotosRoute.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const { uploadImage } = require('../controllers/uploadFotos');
 const upload = multer();
 
 router.post('/upload', upload.single('image'), uploadImage);
