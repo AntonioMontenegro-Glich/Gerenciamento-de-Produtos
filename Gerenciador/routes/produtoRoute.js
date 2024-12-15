e7d9af43f41d1a7c31752d76a7169fe5caf3c72b
AntonioMontenegro-Glich <antoniolemos2004@gmail.com>
2024-12-14 21:47:47 -0300

    lç

--- Gerenciador/routes/produtoRoute.js
+++ Gerenciador/routes/produtoRoute.js
@@ -6,7 +6,7 @@ const produtoController = require('../controllers/produtoController');
 // Rotas do CRUD de produtos
 router.post('/create', upload.single('foto'), produtoController.createProduto);
 router.get('/', produtoController.getAllProdutos);
-router.put('/:id', produtoController.updateProduto);
+router.put('/:id', upload.single('foto'), produtoController.updateProduto);
 router.delete('/:id', produtoController.deleteProduto);
 
 module.exports = router;
